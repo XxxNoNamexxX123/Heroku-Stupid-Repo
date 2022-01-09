9ac2ae47926d3b46f9a87aa7197b3d87613a0ee7
XxxNoNamexxX123 <55279437+XxxNoNamexxX123@users.noreply.github.com>
2022-01-09 21:12:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ bot.on('message', message => {
     }
 })
 
-bot.login("Nzc1MjI2Nzg3OTk0MDA5NjMx.X6jQNQ.jmlzodbksWOvkZGV6JE7T46toWc") //Your own token, thus one sadly won't work :P
+bot.login("NjMyNjg2NzE5ODA3NzgyOTI1.XaJBbA.fmRUc3Yp_d2VtASgohTJCYpzu5U") //Your own token, thus one sadly won't work :P
